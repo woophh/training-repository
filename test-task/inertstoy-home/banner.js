913ab95931354d310a52d53fcb817a6681b461ce
woophh <odoeva.karina@mail.ru>
2024-11-26 16:17:20 +0300

    каталог

---
+++ test-task/inertstoy-home/banner.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+});
